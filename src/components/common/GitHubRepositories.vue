704c1dfb590544705f1e3fe887ea53feabad72ce
jasonlat-dot <lijiaqiangli1024@gmail>
2025-08-10 19:37:37 +0800

    优化404页面

--- src/components/common/GitHubRepositories.vue
+++ src/components/common/GitHubRepositories.vue
@@ -61,7 +61,6 @@
           v-for="(repo, key) in repositories" 
           :key="key"
           class="repository-card"
-          @click="openRepository(repo.url)"
           @mouseenter="playHoverEffect"
         >
         <!-- 卡片背景效果 -->
@@ -100,7 +99,7 @@
         </div>
 
         <!-- 访问按钮 -->
-        <div class="repo-action">
+        <div class="repo-action" @click="openRepository(repo.url)">
           <div class="action-button">
             <svg class="action-icon" viewBox="0 0 24 24" fill="currentColor">
               <path d="M14,3V5H17.59L7.76,14.83L9.17,16.24L19,6.41V10H21V3M19,19H5V5H12V3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V12H19V19Z"/>
